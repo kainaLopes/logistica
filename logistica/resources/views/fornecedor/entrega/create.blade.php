044823c3ff012fd39f07088ca54f7b854996c07e
kaiina-kay <52182761+kaiina-kay@users.noreply.github.com>
2020-09-23 17:26:28 -0400

    resparo no model

--- logistica/resources/views/fornecedor/entrega/create.blade.php
+++ logistica/resources/views/fornecedor/entrega/create.blade.php
@@ -1,1 +1,91 @@
-create
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.min.css')}}">
+  <title>Formulario entrega</title>
+</head>
+<body>
+
+<form>
+    <div class="form-row">
+        <div class="form-group col-md-6">
+            for="datapedido">datapedido</label>
+            <input type="datapedido" class="form-control" id="datapedido" placeholder="datapedido">
+        </div>
+        <div class="form-group col-md-6">
+            for="Email">produto</label>
+            <input type="produto" class="form-control" id="produto" placeholder="produto">
+        </div>
+        <div class="form-group col-md-6">
+            for="quantidadeprodutos">quantidadeprodutos</label>
+            <input type="quantidadeprodutos" class="form-control" id="quantidadeprodutos" placeholder="quantidadeprodutos">
+        </div>
+        <div class="form-group col-md-6">
+            for="categoria">categoria</label>
+            <input type="text" class="form-control" id="categoria" placeholder="categoria">
+        </div>
+        <div class="form-group col-md-6">
+            for="kminicial">kminicial</label>
+            <input type="text" class="form-control" id="kminicial" placeholder="kminicial">
+        </div>
+        <div class="form-group col-md-6">
+            for="kmfinal">KM final</label>
+            <input type="text" class="form-control" id="kmfinal" placeholder="KM final">
+        </div>
+        <div class="form-group col-md-6">
+            for="enderecoentrega">enderecoentrega</label>
+            <input type="enderecoentrega" class="form-control" id="enderecoentrega" placeholder="enderecoentrega">
+        </div>
+        <div class="form-group col-md-6">
+            for="enderecocoleta">enderecocoleta</label>
+            <input type="enderecocoleta" class="form-control" id="enderecocoleta" placeholder="enderecocoleta">
+        </div>
+        <div class="form-group col-md-6">
+            for="datasaida">datasaida</label>
+            <input type="datasaida" class="form-control" id="datasaida" placeholder="datasaida">
+        </div>
+        <div class="form-group col-md-6">
+            for="qtdcombustivel">qtdcombustivel</label>
+            <input type="text" class="form-control" id="qtdcombustivel" placeholder="qtdcombustivel">
+        </div>
+        <div class="form-group col-md-6">
+            for="totaldias">totaldias</label>
+            <input type="text" class="form-control" id="totaldias" placeholder="totaldias">
+        </div>
+        <div class="form-group col-md-6">
+            for="kmrodados">kmrodados</label>
+            <input type="text" class="form-control" id="kmrodados" placeholder="KM rodados">
+        </div>
+        <div class="form-group col-md-6">
+            for="valorbruto">valorbruto</label>
+            <input type="valorbruto" class="form-control" id="valorbruto" placeholder="enderecoentrega">
+        </div>
+        <div class="form-group col-md-6">
+            for="comissaomotorista">comissaomotorista</label>
+            <input type="comissaomotorista" class="form-control" id="comissaomotorista" placeholder="enderecocoleta">
+        </div>
+        <div class="form-group col-md-6">
+            for="valorliquido">valorliquido</label>
+            <input type="valorliquido" class="form-control" id="valorliquido" placeholder="valorliquido">
+        </div>
+        <div class="form-group col-md-6">
+            for="mediaveiculo">mediaveiculo</label>
+            <input type="text" class="form-control" id="mediaveiculo" placeholder="mediaveiculo">
+        </div>
+        <div class="form-group col-md-6">
+            for="datachegada">datachegada</label>
+            <input type="text" class="form-control" id="datachegada" placeholder="datachegada">
+        </div>
+        <div class="form-group col-md-6">
+            for="despesas">despesas</label>
+            <input type="text" class="form-control" id="despesas" placeholder="despesas">
+        </div>
+
+    </div>
+    <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
+
+</body>
+</html>
